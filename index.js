4b24635632cf03ed788d0911342541daa0933733
Edidiva <edikanakpan5@gmail.com>
2023-03-26 12:02:20 +0100

    update

---
+++ index.js
@@ -0,0 +1,11 @@
+//Create a javascript file and inside the file add code that will log in to the console, your name, your preferred programming language, and where you see yourself in the next 1 year.
+ 
+
+function profile(name, language, goals){
+       this.name =name;
+       this.language =language;
+       this.goals= goals;
+}
+
+console.log(new profile('Edikan', 'Javascript', "To be profiecient in programming and landed a paid internship with tech unicorn compainies as well as a full time role and living my best life"));
+
